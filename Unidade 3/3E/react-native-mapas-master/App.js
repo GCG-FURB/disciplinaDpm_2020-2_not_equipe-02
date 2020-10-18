5a7242138c36f8cc5015359e62f1111870b2aa3f
Carol Regalin <carol_regalin@hotmail.com>
2020-10-18 00:38:37 -0300

    3E - Localização atual no mapa

---
+++ Unidade 3/3E/react-native-mapas-master/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+import customStyle from './customStyle';
+
+export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.04
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001
+      });
+
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  console.log(location);
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        customMapStyle={customStyle}
+        style={styles.mapStyle}
+        region={region}
+     />
+      <Marker
+          coordinate={region}
+          title="Local de Entrega"
+          description="Ruas das Palmeiras, 25"
+          icon={{
+            uri: "https://img.icons8.com/plasticine/1x/truck.png"
+          }}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: '100%'
+  }
+});
