c9881f12bedca4820181e76f2d7db0a89aed0265
Carol Regalin <carol_regalin@hotmail.com>
2020-11-02 20:18:12 -0300

    3H

---
+++ Unidade 3/gitapi3H/src/components/Repository/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Container, Name, Description, Stats, Stat, StatCount} from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function Repository({data}) {
+  return (
+    <Container>
+      <Name>{data.name}</Name>
+      <Description>{data.description}</Description>
+      <Stats>
+        <Stat>
+          <Icon name="star" size={16} color="#333" />
+          <StatCount>{data.stars}</StatCount>
+        </Stat>
+        <Stat>
+          <Icon name="code-fork" size={16} color="#333" />
+          <StatCount>{data.forks}</StatCount>
+        </Stat>
+      </Stats>
+    </Container>
+  );
+}
