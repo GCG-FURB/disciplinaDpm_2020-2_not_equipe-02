7583654329637379a50ffee20e7c31bf7ce25739
Carol Regalin <caroline.regalin@gett.com.br>
2020-12-01 20:05:25 -0300

    Pegando a posição atual do usuário para obter temperatura

--- Unidade 4/metereologiaApp/src/pages/Main/index.js
+++ Unidade 4/metereologiaApp/src/pages/Main/index.js
@@ -1,27 +1,72 @@
 import React, {useState, useEffect} from 'react';
-import {Keyboard} from 'react-native';
+import {Keyboard, PermissionsAndroid} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Container, Title, Form, Input, Submit, List} from './styles';
 import api from '~/services/api';
 import getRealm from '~/services/realm';
 import Weather from '~/components/Weather';
+import Geolocation from 'react-native-geolocation-service';
 
 export default function Main() {
   const [input, setInput] = useState('');
   const [error, setError] = useState(false);
   const [weathers, setWeathers] = useState([]);
+  const [position, setPosition] = useState(null);
 
   useEffect(() => {
     async function loadWeathers() {
       const realm = await getRealm();
       const data = realm.objects('Weather');
       setWeathers(data);
-    }
 
-    console.log(weathers);
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        );
+
+        if (granted) {
+          console.log('You can use the ACCESS_FINE_LOCATION');
+
+          Geolocation.getCurrentPosition(
+            async (position) => {
+              console.log(position);
+              const response = await fetch(
+                `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyA8dThs16mRcmgEuorMMnp0CuH22VQNriw`,
+              );
+              const json = await response.json();
+              handlerRefreshWeatherForecast(json.results[0].address_components[3].long_name);
+            },
+            (error) => {
+              // See error code charts below.
+              console.log(error.code, error.message);
+            },
+            {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+          );
+        } else {
+          console.log('ACCESS_FINE_LOCATION permission denied');
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    }
 
     loadWeathers();
   }, []);
+
+  async function getLocationUser() {
+    await Geolocation.getCurrentPosition(
+      (info) => {
+        setPosition({
+          longitude: info.coords.longitude,
+          latitude: info.coords.latitude,
+        });
+        console.log(position);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  }
 
   async function saveForecast(params) {
     const data = {
@@ -40,11 +85,12 @@ export default function Main() {
     return data;
   }
 
-
   async function handleCityWeatherForecast() {
     try {
       console.log(input);
-      const response = await api.get(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`);
+      const response = await api.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`,
+      );
 
       await saveForecast(response.data);
       setInput('');
@@ -56,15 +102,14 @@ export default function Main() {
     }
   }
 
-  async function handlerRefreshWeatherForecast(weather) {
-    const response = await api.get(`https://api.openweathermap.org/data/2.5/weather?q=${weather.name}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`);
+  async function handlerRefreshWeatherForecast(name) {
+    const response = await api.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`,
+    );
     console.log(response);
     const data = await saveForecast(response.data);
 
-    setWeathers(
-      weathers.map((temp) => (temp.id === data.id ? data : temp)),
-    );
-
+    setWeathers(weathers.map((temp) => (temp.id === data.id ? data : temp)));
   }
 
   return (
@@ -91,11 +136,10 @@ export default function Main() {
         renderItem={({item}) => (
           <Weather
             data={item}
-            onRefresh={() => handlerRefreshWeatherForecast(item)}
+            onRefresh={() => handlerRefreshWeatherForecast(item.name)}
           />
         )}
-      /> 
-
+      />
     </Container>
   );
 }
