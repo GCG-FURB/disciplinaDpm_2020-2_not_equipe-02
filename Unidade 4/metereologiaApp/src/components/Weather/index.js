c9c960e85a58a7e3fe2bdeefd078ee1816ed34fb
Carol Regalin <caroline.regalin@gett.com.br>
2020-12-03 22:19:38 -0300

    Alterando cores

--- Unidade 4/metereologiaApp/src/components/Weather/index.js
+++ Unidade 4/metereologiaApp/src/components/Weather/index.js
@@ -11,24 +11,13 @@ import {
   RefreshText,
 } from './styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 export default function Weather({data, onRefresh}) {
   return (
     <Container>
       <Name>{data.name}</Name>
       <Description>{data.temp}</Description>
-      {/* <Stats>
-        <Stat>
-          <Icon name="star" size={16} color="#333" />
-          <StatCount>{data.stars}</StatCount>
-        </Stat>
-        <Stat>
-          <Icon name="code-fork" size={16} color="#333" />
-          <StatCount>{data.forks}</StatCount>
-        </Stat>
-      </Stats> */}
       <Refresh onPress={onRefresh}>
-        <Icon name="refresh" color="#7159c1" size={16} />
+        <Icon name="refresh" color="#49c1ab" size={16} />
         <RefreshText>ATUALIZAR</RefreshText>
       </Refresh>
     </Container>
